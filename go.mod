e3ae49daa074835698aeadd7a295f90ee9c63c7c
Tim Schindler <xh3b4sd@gmail.com>
2020-10-06 18:43:39 +0200

    add apiserver implementation

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/xh3b4sd/apiserver
+
+go 1.15
+
+require (
+	github.com/xh3b4sd/gocode v0.0.0-20201005175120-c7576bd802e8
+	github.com/xh3b4sd/tracer v0.3.1
+	google.golang.org/grpc v1.32.0
+)
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/xh3b4sd/tracer"
+	"google.golang.org/grpc"
+
+	"github.com/xh3b4sd/gocode/pkg/pbf/post"
+)
+
+func main() {
+	err := mainE(context.Background())
+	if err != nil {
+		tracer.Panic(err)
+	}
+}
+
+func mainE(ctx context.Context) error {
+	var err error
+
+	var l net.Listener
+	{
+		l, err = net.Listen("tcp", fmt.Sprintf(":%d", 7777))
+		if err != nil {
+			return tracer.Mask(err)
+		}
+	}
+
+	var a post.APIServer
+	{
+		a = &API{}
+
+		g := grpc.NewServer()
+		post.RegisterAPIServer(g, a)
+		err := g.Serve(l)
+		if err != nil {
+			return tracer.Mask(err)
+		}
+	}
+
+	return nil
+}
+
+// -------------------------------------------------------------------------- //
+
+type API struct {
+	post.UnimplementedAPIServer
+}
+
+func (a *API) Create(ctx context.Context, cre *post.CreateI) (*post.CreateO, error) {
+	fmt.Printf("%#v\n", cre)
+	return &post.CreateO{Message: "create output"}, nil
+}
+
+func (a *API) Delete(ctx context.Context, del *post.DeleteI) (*post.DeleteO, error) {
+	fmt.Printf("%#v\n", del)
+	return &post.DeleteO{Message: "delete output"}, nil
+}
+
+func (a *API) Search(ctx context.Context, sea *post.SearchI) (*post.SearchO, error) {
+	fmt.Printf("%#v\n", sea)
+	return &post.SearchO{Message: "search output"}, nil
+}
+
+func (a *API) Update(ctx context.Context, upd *post.UpdateI) (*post.UpdateO, error) {
+	fmt.Printf("%#v\n", upd)
+	return &post.UpdateO{Message: "update output"}, nil
+}
